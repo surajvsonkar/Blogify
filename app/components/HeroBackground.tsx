880847e26395814a01eb29038b51f40f71da9dc4
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-08-08 01:41:33 +0530

    project init

--- app/components/HeroBackground.tsx
+++ app/components/HeroBackground.tsx
@@ -1,6 +1,6 @@
 import Spline from '@splinetool/react-spline/next';
 
-export default function Home() {
+export default function HeroBackground() {
 	return (
 		<main>
 			<Spline scene="https://prod.spline.design/TuPmR9TNpMCpAtBp/scene.splinecode" />
