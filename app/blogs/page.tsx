829a175b6b754a0a98c9cc5197f4b87e8264fecd
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-08-20 03:29:41 +0530

    schema updated and updated the blog component

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -7,10 +7,17 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from '@/components/ui/select';
-import { Search } from 'lucide-react';
+import { Calendar, Heart, Search, User } from 'lucide-react';
 import { PrismaClient } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 
 export default async function BlogPage() {
 	const prisma = new PrismaClient();
@@ -18,13 +25,33 @@ export default async function BlogPage() {
 	// Get session on server
 	const session = await getServerSession(authOptions);
 	const userId = Number(session?.user?.id);
-	console.log(userId);
+	// console.log(userId);
 
-	// Fetch posts server-side
-	const posts = await prisma.blog.findMany({
+	const truncateWords = (str: string) => {
+		const words = str.split(' ');
+		if (words.length <= 50) {
+			return str;
+		}
+		return words.slice(0, 30).join(' ') + '...';
+	};
+
+	// Fetch blogs server-side
+	const blogs = await prisma.blog.findMany({
 		where: { authorId: { not: userId }, isPublic: true },
+		select: {
+			author: {
+				select: {
+					username: true,
+				},
+			},
+			likes: true,
+			title: true,
+			description: true,
+			id: true,
+			createdAt: true,
+		},
 	});
-	// console.log(posts)
+	// console.log(blogs);
 
 	return (
 		<div className="min-h-screen">
@@ -35,7 +62,7 @@ export default async function BlogPage() {
 			<div className="container mx-auto mt-8 px-4 py-8">
 				<div className="max-w-4xl mx-auto">
 					<div className="mb-8">
-						<h1 className="text-3xl font-montserrat font-bold">All Posts</h1>
+						<h1 className="text-3xl font-montserrat font-bold">All Blogs</h1>
 						<p className="font-montserrat font-semibold mt-2 text-gray-500">
 							Discover stories, insights, and ideas from our community
 						</p>
@@ -43,7 +70,7 @@ export default async function BlogPage() {
 					<div className="flex flex-col sm:flex-row mb-8 gap-4">
 						<div className="relative flex-1">
 							<Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-muted-foreground" />
-							<Input placeholder="Search Posts...." className="pl-10" />
+							<Input placeholder="Search Blogs...." className="pl-10" />
 						</div>
 						<Select>
 							<SelectTrigger>
@@ -56,12 +83,39 @@ export default async function BlogPage() {
 							</SelectContent>
 						</Select>
 					</div>
-					{posts.map((post) => (
-						<div key={post.id} className="mb-4 border p-4 rounded">
-							<h2 className="text-xl font-semibold">{post.title}</h2>
-							<p>{post.description}</p>
-						</div>
-					))}
+					<div className="flex flex-col gap-4">
+						{blogs.map((blog) => (
+							<Card
+								key={blog.id}
+								className="hover:shadow-lg transition-shadow group"
+							>
+								<CardHeader>
+									<CardTitle>{blog.title}</CardTitle>
+									<CardDescription>
+										{truncateWords(blog.description)}
+									</CardDescription>
+								</CardHeader>
+								<CardContent>
+									<div className="flex justify-end gap-4 items-center">
+										<div className="flex items-center gap-1 tesm">
+											<Heart className="w-3 h-3" />
+											<span>{blog.likes.length}</span>
+										</div>
+										<div className="flex items-center gap-1 tesm">
+											<User className="w-3 h-3" />
+											<span>{blog.author.username}</span>
+										</div>
+										<div className="flex items-center gap-1 tesm">
+											<Calendar className="w-3 h-3" />
+											<span>
+												{String(blog.createdAt.toISOString().split('T')[0])}
+											</span>
+										</div>
+									</div>
+								</CardContent>
+							</Card>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
