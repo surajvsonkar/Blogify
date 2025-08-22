4dccdfcc99e54d238f68a961451a307432d5fee7
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-08-22 21:14:04 +0530

    completed the overview tab

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,16 @@
 'use client';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 import Header from '@/components/ui/header';
+import { Input } from '@/components/ui/input';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { authOptions } from '@/lib/auth';
-import { FileText, Plus } from 'lucide-react';
+import { FileText, Plus, Search } from 'lucide-react';
 import { getServerSession } from 'next-auth';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -12,7 +19,7 @@ import { useState } from 'react';
 export default function Dashboard() {
 	const session = useSession();
 	console.log(session);
-	const [selectedTab, setSelectedTab] = useState('content');
+	const [selectedTab, setSelectedTab] = useState('overview');
 	return (
 		<div className="min-h-screen">
 			<Header
@@ -39,32 +46,119 @@ export default function Dashboard() {
 					</div>
 					<Tabs
 						value={selectedTab}
-						className="space-y-6"
+						className="space-y-6 mb-6"
 						onValueChange={setSelectedTab}
 					>
-						<TabsList>
-							<TabsTrigger value="content">content</TabsTrigger>
-							<TabsTrigger value="content2">content2</TabsTrigger>
-							<TabsTrigger value="content3">content3</TabsTrigger>
+						<TabsList className="grid w-full grid-cols-3 lg:w-96 font-montserrat font-bold">
+							<TabsTrigger value="overview">Overview</TabsTrigger>
+							<TabsTrigger value="blog">Blogs</TabsTrigger>
+							<TabsTrigger value="analytics">Analytics</TabsTrigger>
 						</TabsList>
 
-						<TabsContent value="content" className="space-y-6">
+						<TabsContent value="overview" className="space-y-6">
 							<div className="grid grid-cols-4 gap-4">
 								<Card>
 									<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-										<CardTitle className='text-xs font-medium'>Total Posts</CardTitle>
-										<FileText className='h-4 w-4 text-muted-foreground' />
+										<CardTitle className="text-xs font-medium">
+											Total Posts
+										</CardTitle>
+										<FileText className="h-4 w-4 text-muted-foreground" />
 									</CardHeader>
 									<CardContent>
 										<div className="text-2xl font-bold">24</div>
-										<p className='text-xs text-muted-foreground'>+2 from last month</p>
+										<p className="text-xs text-muted-foreground">
+											+2 from last month
+										</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+										<CardTitle className="text-xs font-medium">
+											Total Posts
+										</CardTitle>
+										<FileText className="h-4 w-4 text-muted-foreground" />
+									</CardHeader>
+									<CardContent>
+										<div className="text-2xl font-bold">24</div>
+										<p className="text-xs text-muted-foreground">
+											+2 from last month
+										</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+										<CardTitle className="text-xs font-medium">
+											Total Posts
+										</CardTitle>
+										<FileText className="h-4 w-4 text-muted-foreground" />
+									</CardHeader>
+									<CardContent>
+										<div className="text-2xl font-bold">24</div>
+										<p className="text-xs text-muted-foreground">
+											+2 from last month
+										</p>
+									</CardContent>
+								</Card>
+								<Card>
+									<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+										<CardTitle className="text-xs font-medium">
+											Total Posts
+										</CardTitle>
+										<FileText className="h-4 w-4 text-muted-foreground" />
+									</CardHeader>
+									<CardContent>
+										<div className="text-2xl font-bold">24</div>
+										<p className="text-xs text-muted-foreground">
+											+2 from last month
+										</p>
 									</CardContent>
 								</Card>
 							</div>
+							<Card className="bg-accent">
+								<CardHeader>
+									<CardTitle>Recent Activity</CardTitle>
+									<CardDescription>Latest updates on your blog</CardDescription>
+								</CardHeader>
+								<CardContent>
+									<Link
+										href={''}
+										className="flex justify-between mb-4 hover:bg-white p-2"
+									>
+										<p>New comment onGetting Started with Next.js</p>
+										<span>2 days ago</span>
+									</Link>
+									<Link href={''} className="flex justify-between mb-4">
+										<p>New comment onGetting Started with Next.js</p>
+										<span>2 days ago</span>
+									</Link>
+									<Link href={''} className="flex justify-between mb-4">
+										<p>New comment onGetting Started with Next.js</p>
+										<span>2 days ago</span>
+									</Link>
+									<Link href={''} className="flex justify-between mb-4">
+										<p>New comment onGetting Started with Next.js</p>
+										<span>2 days ago</span>
+									</Link>
+									<Link href={''} className="flex justify-between mb-4">
+										<p>New comment onGetting Started with Next.js</p>
+										<span>2 days ago</span>
+									</Link>
+								</CardContent>
+							</Card>
 						</TabsContent>
+
+                        <TabsContent value='blog' className='space-y-6'>
+                            <div className='flex'>
+                                <div className='relative flex-1'>
+                                    <Search className='w-4 h-4 transform -translate-y-1/2 absolute left-3 top-1/2'/>
+                                    <Input placeholder='Search Blogs....' className='pl-10'/>
+                                </div>
+                            </div>
+                        </TabsContent>
 					</Tabs>
 				</div>
 			</div>
 		</div>
 	);
 }
+Card;
